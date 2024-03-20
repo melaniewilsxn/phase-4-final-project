152344f68e66c464422ec5059e5071afae7af326
Melanie Wilson <melaniemwilson1@gmail.com>
2024-03-20 09:45:12 -0400

    added elements

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Header from "./Header"
+import About from "../pages/About";
+import Account from "../pages/Account";
+import Discover from "../pages/Discover";
+import Home from "../pages/Home";
+import Library from "../pages/Library";
+import NavBar from "./NavBar";
 
 function App() {
-  return <h1>Project Client</h1>;
+  // const [user, setUser] = useState(null)
+
+  // useEffect(() => {
+  //   fetch("/check_session")
+  //   .then((r) => {
+  //     if(r.ok){
+  //       r.json().then((user) => setUser(userf))
+  //     }
+  //   })
+  // }, [])
+
+  // if(!user) return <Login onLogin={setUser} />
+
+  return (
+    <div className="App">
+      <Header />
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/library">
+          <Library />
+        </Route>
+        <Route exact path="/discover">
+          <Discover />
+        </Route>
+        <Route exact path="/account">
+          <Account />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import App from "./components/App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css';
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+  </BrowserRouter>
+);
